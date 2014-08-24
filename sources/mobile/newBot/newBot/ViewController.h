85fc6e617ac8df90f75dd329d030a91243c011d3
Raghunanandan <raghunanandan@os-x-server.local>
2014-08-24 13:29:28 +0530

    t

---
+++ sources/mobile/newBot/newBot/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  newBot
+//
+//  Created by Raghunanandan on 24/08/14.
+//  Copyright (c) 2014 raghu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ sources/mobile/newBot/newBot/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  newBot
+//
+//  Created by Raghunanandan on 24/08/14.
+//  Copyright (c) 2014 raghu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
